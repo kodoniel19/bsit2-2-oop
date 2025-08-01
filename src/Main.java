112241b3fedac65cd4230735fe21bb5eb6fbb716
Doniel Eunson Ko <deko74415@liceo.edu.ph>
2025-08-01 09:36:50 +0800

    labact2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number 1: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter number 2: ");
+        int num2 = input.nextInt();
+
+        System.out.println("Enter number 3: ");
+        int num3 = input.nextInt();
+
+        System.out.println("Enter number 4: ");
+        int num4 = input.nextInt();
+
+        System.out.println("Enter number 5: ");
+        int num5 = input.nextInt();
+
+        int totalNum = num1 + num2 + num3 +num4 + num5;
+
+        System.out.println(" ");
+        System.out.println("Total Number: " + totalNum);
+
     }
 }
