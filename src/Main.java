3851b5cb6222693b23c4c61a4a4f2a865dfabe2f
Doniel Eunson Ko <deko74415@liceo.edu.ph>
2025-08-01 09:54:11 +0800

    labact2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-// loop ver.
-
 import java.util.Scanner;
 
 public class Main {
@@ -18,33 +16,3 @@ public class Main {
         System.out.println("Total Number: " + totalNum);
     }
 }
-
-// not loop ver.
-
-//import java.util.Scanner;
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//
-//        System.out.println("Enter number 1: ");
-//        int num1 = input.nextInt();
-//
-//        System.out.println("Enter number 2: ");
-//        int num2 = input.nextInt();
-//
-//        System.out.println("Enter number 3: ");
-//        int num3 = input.nextInt();
-//
-//        System.out.println("Enter number 4: ");
-//        int num4 = input.nextInt();
-//
-//        System.out.println("Enter number 5: ");
-//        int num5 = input.nextInt();
-//
-//        int totalNum = num1 + num2 + num3 +num4 + num5;
-//
-//        System.out.println(" ");
-//        System.out.println("Total Number: " + totalNum);
-//
-//    }
-//}
