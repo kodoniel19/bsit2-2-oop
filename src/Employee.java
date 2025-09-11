7680e8bf54c0d38a46c09bc1ce3772dc072af247
Doniel Eunson Ko <167674608+kodoniel19@users.noreply.github.com>
2025-09-11 11:15:05 +0800

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -15,7 +15,7 @@ public class Employee {
     public void displayInfo() {
         System.out.println("Name: " + name);
         System.out.println("Employee ID: " + employeeId);
-        System.out.println("Base Salary: $" + baseSalary + ".0");
+        System.out.println("Base Salary: $" + baseSalary);
         System.out.println("Department: " + department);
     }
 
