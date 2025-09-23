e1d089e02f12f9b03526e70caa9b142f3fc314a3
Doniel Eunson Ko <167674608+kodoniel19@users.noreply.github.com>
2025-09-23 21:36:31 +0800

    Create LibraryUser.java

---
+++ src/LibraryUser.java
@@ -0,0 +1,102 @@
+interface LibraryUser {
+    String getUserType();
+    void borrowMedia(Media media);
+    void returnMedia(Media media);
+    int getMaxBorrowLimit();
+}
+
+
+class Student implements LibraryUser {
+    private String name;
+    private String studentId;
+
+    public Student(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    public String getUserType() {
+        return "Student";
+    }
+
+    public int getMaxBorrowLimit() {
+        return 3;
+    }
+
+    public void borrowMedia(Media media) {
+        if (media.isAvailable()) {
+            media.borrowItem();
+            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
+        } else {
+            System.out.println("Sorry, " + media.getTitle() + " is not available");
+        }
+    }
+
+    public void returnMedia(Media media) {
+        media.returnItem();
+    }
+}
+
+
+class Teacher implements LibraryUser {
+    private String name;
+    private String employeeId;
+
+    public Teacher(String name, String employeeId) {
+        this.name = name;
+        this.employeeId = employeeId;
+    }
+
+    public String getUserType() {
+        return "Teacher";
+    }
+
+    public int getMaxBorrowLimit() {
+        return 5;
+    }
+
+    public void borrowMedia(Media media) {
+        if (media.isAvailable()) {
+            media.borrowItem();
+            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
+        } else {
+            System.out.println("Sorry, " + media.getTitle() + " is not available");
+        }
+    }
+
+    public void returnMedia(Media media) {
+        media.returnItem();
+    }
+}
+
+
+class Librarian implements LibraryUser {
+    private String name;
+    private String employeeId;
+
+    public Librarian(String name, String employeeId) {
+        this.name = name;
+        this.employeeId = employeeId;
+    }
+
+    public String getUserType() {
+        return "Librarian";
+    }
+
+    public int getMaxBorrowLimit() {
+        return 10;
+    }
+
+    public void borrowMedia(Media media) {
+        if (media.isAvailable()) {
+            media.borrowItem();
+            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
+        } else {
+            System.out.println("Sorry, " + media.getTitle() + " is not available");
+        }
+    }
+
+    public void returnMedia(Media media) {
+        media.returnItem();
+    }
+}
